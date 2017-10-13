95f1ebe87e993db1a997a97f005d021b70eec052
Dina Chanthan <chanthandian@gmail.com>
2017-10-14 04:06:11 +0700

    render blog detail page

--- routes/web.php
+++ routes/web.php
@@ -14,26 +14,26 @@
 
 Route::group(['middleware' =>'locale'],function(){
   Route::get('/', 'PageController@home');
-  Route::get('/contacts', 'PageController@contact');
-  Route::get('/about','PageController@about');
-  Route::get('/team','PageController@team');
-  Route::get('/services','PageController@service');
-  Route::get('/report','PageController@report');
-  Route::get('/blog','PageController@blog');
-  Route::get('/blog/{slug}','PageController@blogDetail')->name('visitor.blog.detail');
-  Route::get('/publication','PageController@publication');
-  Route::get('/law','PageController@law');
-  Route::get('/announcement','PageController@announcement');
-  Route::get('/training','PageController@training');
-  Route::get('/employment_and_internship','PageController@employment');
-  Route::get('/mission','PageController@mission');
-  Route::get('/teamsingle','PageController@teamsingle');
-  Route::post('/mail','System@sendmail');
+  Route::get('contacts', 'PageController@contact');
+  Route::get('about','PageController@about');
+  Route::get('team','PageController@team');
+  Route::get('services','PageController@service');
+  Route::get('report','PageController@report');
+  Route::get('blog','PageController@blog');
+  Route::get('blog/{slug}','PageController@blogDetail')->name('visitor.blog.detail');
+  Route::get('publication','PageController@publication');
+  Route::get('law','PageController@law');
+  Route::get('announcement','PageController@announcement');
+  Route::get('training','PageController@training');
+  Route::get('employment_and_internship','PageController@employment');
+  Route::get('mission','PageController@mission');
+  Route::get('teamsingle','PageController@teamsingle');
+  Route::post('mail','System@sendmail');
 });
 
 Route::get('locale/{localeId?}','LocaleController@locales');
 Route::get('PDF/storage/law-files/{date}/{path}','System@downloadpdf');
-Route::post('/mail','System@sendmail');
+Route::post('mail','System@sendmail');
 
 
 Route::group(['prefix' => 'admin'], function () {
