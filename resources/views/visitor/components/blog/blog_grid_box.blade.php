f8b9e94c34359e188cf21040afa240bff410bdd5
Dina Chanthan <chanthandian@gmail.com>
2017-10-14 03:09:05 +0700

    render blog page

---
+++ resources/views/visitor/components/blog/blog_grid_box.blade.php
@@ -0,0 +1,29 @@
+<div class="element col-xs-12 col-sm-6 col-md-4">
+    <div class="blog-item center-block">
+        <div class="inner">
+            <figure class="img-wrap">
+                <img class="img-responsive" src="{{ asset('/storage/'.$post->image) }}" alt="{{ $post->getTranslatedAttribute('title', $locale) }}" />
+            </figure>
+
+            <a href="{{ route('visitor.blog.detail', $post->slug) }}"></a>
+        </div>
+
+        <div class="description">
+            <span class="date-post">POSTED {{ $post->created_at->format('M d\, Y') }} </span>
+
+            <h3 class="title"><a href="{{ route('visitor.blog.detail', $post->slug) }}">{{ $post->getTranslatedAttribute('title', $locale) }}</a></h3>
+
+            <div class="meta">
+                <span><i class="icon-user"></i><a href="#">{{ $post->authorId->name }}</a></span>
+
+                <span><i class="icon-comment"></i><a href="#">0 Comment</a></span>
+            </div>
+
+            <p>
+                {{ $post->getTranslatedAttribute('excerpt', $locale) }}
+            </p>
+
+            <a class="custom-btn medium dark-color" href="{{ route('visitor.blog.detail', $post->slug) }}" data-text="Continue Reading"><span>Continue Reading</span></a>
+        </div>
+    </div>
+</div>
--- resources/views/visitor/pages/blog/blog.blade.php
+++ resources/views/visitor/pages/blog/blog.blade.php
@@ -40,195 +40,30 @@
 							<div class="row js-isotope" data-isotope-options='{"itemSelector": ".element", "transitionDuration": "0.8s", "percentPosition": "true", "masonry": { "columnWidth": "#grid-sizer" }}'>
 								<div id="grid-sizer" class="col-xs-12 col-sm-6 col-md-4" style="min-height: 0;"></div>
 
-								<div class="element col-xs-12 col-sm-6 col-md-4">
-									<div class="blog-item center-block">
-										<div class="inner">
-											<figure class="img-wrap">
-												<img class="img-responsive" src="../img/blog_img/4.jpg" alt="demo" />
-											</figure>
-
-											<a href="blog_post.html"></a>
-										</div>
-
-										<div class="description">
-											<span class="date-post">POSTED JULY 17, 2014 </span>
-
-											<h3 class="title"><a href="blog_post.html">The court case of Adam Clark</a></h3>
-
-											<div class="meta">
-												<span><i class="icon-user"></i><a href="#">Admin</a></span>
-
-												<span><i class="icon-comment"></i><a href="#">1 Comment</a></span>
-											</div>
-
-											<p>
-												There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't
-											</p>
-
-											<a class="custom-btn medium dark-color" href="blog_post.html" data-text="Continue Reading"><span>Continue Reading</span></a>
-										</div>
-									</div>
-								</div>
-
-								<div class="element col-xs-12 col-sm-6 col-md-4">
-									<div class="blog-item center-block">
-										<div class="inner">
-											<figure class="img-wrap">
-												<img class="img-responsive" src="../img/blog_img/5.jpg" alt="demo" />
-											</figure>
-
-											<a href="blog_post.html"></a>
-										</div>
-
-										<div class="description">
-											<span class="date-post">POSTED JULY 17, 2014 </span>
-
-											<h3 class="title"><a href="blog_post.html">Lawyers  across states</a></h3>
-
-											<div class="meta">
-												<span><i class="icon-user"></i><a href="#">Admin</a></span>
-
-												<span><i class="icon-comment"></i><a href="#">1 Comment</a></span>
-											</div>
-
-											<p>
-												There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even
-											</p>
-
-											<a class="custom-btn medium dark-color" href="blog_post.html" data-text="Continue Reading"><span>Continue Reading</span></a>
-										</div>
-									</div>
-								</div>
-
-								<div class="element col-xs-12 col-sm-6 col-md-4">
-									<div class="blog-item center-block">
-										<div class="inner">
-											<figure class="img-wrap">
-												<img class="img-responsive" src="../img/blog_img/1.jpg" alt="demo" />
-											</figure>
-
-											<a href="blog_post.html"></a>
-										</div>
-
-										<div class="description">
-											<span class="date-post">POSTED JULY 17, 2014 </span>
-
-											<h3 class="title"><a href="blog_post.html">Lawyer  Year Award</a></h3>
-
-											<div class="meta">
-												<span><i class="icon-user"></i><a href="#">Admin</a></span>
-
-												<span><i class="icon-comment"></i><a href="#">1 Comment</a></span>
-											</div>
-
-											<p>
-												There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even
-											</p>
-
-											<a class="custom-btn medium dark-color" href="blog_post.html" data-text="Continue Reading"><span>Continue Reading</span></a>
-										</div>
-									</div>
-								</div>
-
-								<div class="element col-xs-12 col-sm-6 col-md-4">
-									<div class="blog-item center-block">
-										<div class="inner">
-											<figure class="img-wrap">
-												<img class="img-responsive" src="../img/blog_img/2.jpg" alt="demo" />
-											</figure>
-
-											<a href="blog_post.html"></a>
-										</div>
-
-										<div class="description">
-											<span class="date-post">POSTED JULY 17, 2014 </span>
-
-											<h3 class="title"><a href="blog_post.html">Duis volutpat magna</a></h3>
-
-											<div class="meta">
-												<span><i class="icon-user"></i><a href="#">Admin</a></span>
-
-												<span><i class="icon-comment"></i><a href="#">1 Comment</a></span>
-											</div>
-
-											<p>
-												There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even
-											</p>
-
-											<a class="custom-btn medium dark-color" href="blog_post.html" data-text="Continue Reading"><span>Continue Reading</span></a>
-										</div>
-									</div>
-								</div>
-
-								<div class="element col-xs-12 col-sm-6 col-md-4">
-									<div class="blog-item center-block">
-										<div class="inner">
-											<figure class="img-wrap">
-												<img class="img-responsive" src="../img/blog_img/6.jpg" alt="demo" />
-											</figure>
-
-											<a href="blog_post.html"></a>
-										</div>
-
-										<div class="description">
-											<span class="date-post">POSTED JULY 17, 2014 </span>
-
-											<h3 class="title"><a href="blog_post.html">Maecenas tincidunt nisi quis nunc</a></h3>
-
-											<div class="meta">
-												<span><i class="icon-user"></i><a href="#">Admin</a></span>
-
-												<span><i class="icon-comment"></i><a href="#">1 Comment</a></span>
-											</div>
-
-											<p>
-												There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even
-											</p>
-
-											<a class="custom-btn medium dark-color" href="blog_post.html" data-text="Continue Reading"><span>Continue Reading</span></a>
-										</div>
-									</div>
-								</div>
-
-								<div class="element col-xs-12 col-sm-6 col-md-4">
-									<div class="blog-item center-block">
-										<div class="description">
-											<span class="date-post">POSTED JULY 17, 2014 </span>
-
-											<h3 class="title"><a href="blog_post.html">Maecenas tincidunt nisi quis nunc</a></h3>
-
-											<div class="meta">
-												<span><i class="icon-user"></i><a href="#">Admin</a></span>
-
-												<span><i class="icon-comment"></i><a href="#">1 Comment</a></span>
-											</div>
-
-											<p>
-												There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even
-											</p>
-
-											<a class="custom-btn medium dark-color" href="blog_post.html" data-text="Continue Reading"><span>Continue Reading</span></a>
-										</div>
-									</div>
-								</div>
+								@if(isset($latest_blogs) && count($latest_blogs) > 0)
+									@foreach($latest_blogs as $post)
+										@includeIf('visitor.components.blog.blog_grid_box', ['post' => $post])
+									@endforeach
+								@endif
+								
 							</div>
 						</div>
 
-						<div class="text-center">
+						<!-- <div class="text-center">
 							<a class="control-btn control-btn-style-1 prev-btn icon-left" href="#"></a>
 							<a class="control-btn control-btn-style-1 next-btn icon-right" href="#"></a>
-						</div>
+						</div> -->
 					</div>
 				</div>
 			</section>
 		</main>
 
-	@endsection
-	@push('scripts')
-	@endpush
-	@section('scripts')
-	<script>
-		$(document).ready(function(){
-		});
-	</script>
-	@endsection
+@endsection
+@push('scripts')
+@endpush
+@section('scripts')
+<script>
+	$(document).ready(function(){
+	});
+</script>
+@endsection
