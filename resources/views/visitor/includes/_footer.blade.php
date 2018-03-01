14daf7624ff06967275d07b4b13d5bfc38cef63a
SreynethKong <sreynethkong@gmail.com>
2018-03-01 17:34:10 +0700

    address

--- resources/views/visitor/includes/_footer.blade.php
+++ resources/views/visitor/includes/_footer.blade.php
@@ -5,9 +5,18 @@
                 <div class="row">
                     <div class="col-xs-12 col-sm-5">
                         <div class="footer-item">
-                           <h3 class="title">@lang('text.contact_us')</h3>
+                           <h3 class="title">@lang('text.Address')</h3>
                             <address>
-                                <span>{{ Voyager::setting('company_address_kh') }} {{$locale}}</span>
+                               @if($locale == 'en')
+                                  <span>{{ Voyager::setting('company_address_en') }} </span>
+                               @endif
+                               @if($locale == 'kh')
+                                  <span>{{ Voyager::setting('company_address_kh') }} </span>
+                               @endif
+                               @if($locale == 'ch')
+                                  <span>{{ Voyager::setting('company_address_ch') }} </span>
+                               @endif
+
                                 <br />
                                 <span>@lang('text.Phone'): {{ Voyager::setting('company_tel') }}</span>
                                 <span>@lang('text.fax'): {{ Voyager::setting('company_fax') }}</span>
