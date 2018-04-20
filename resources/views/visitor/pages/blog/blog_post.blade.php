627a9a08cb2f9959ff7ec972a2b5fee3cceb5c13
SreynethKong <sreynethkong@gmail.com>
2018-04-20 15:25:06 +0700

    post

--- resources/views/visitor/pages/blog/blog_post.blade.php
+++ resources/views/visitor/pages/blog/blog_post.blade.php
@@ -7,7 +7,7 @@
 <meta name="keyword" content="">
 <meta property="og:url" content="http://www.skpcambodia.com/{{ Request::path() }}" />
 <meta name="og:type" content="article">
-<meta name="og:title" content="{{ $post->getTranslatedAttribute('title', $locale) }}">
+<meta name="og:title" content="{{ $post->title }}">
 <meta name="og:image" content="">
 <meta name="og:description" content="">
 @endpush
